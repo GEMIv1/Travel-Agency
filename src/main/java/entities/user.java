e3937f16763d6c30da87ef90e2a1a4756889a80b
moyasser20 <mohamedyasser192023@gmail.com>
2024-12-14 00:46:33 +0200

    Changes in the user entitiy class

--- src/main/java/entities/user.java
+++ src/main/java/entities/user.java
@@ -7,7 +7,44 @@ public class user {
     private String age;
     private String phoneNumber;
 
-    
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
