7fe8aee99c794353afe50efff2691a5645e56fef
GEMI <ahmed.mohamedmed1111@gmail.com>
2024-12-14 01:06:31 +0200

    Updated controllers

--- src/main/java/com/example/Travel_agency/controllers/userManagmentCtr.java
+++ src/main/java/com/example/Travel_agency/controllers/userManagmentCtr.java
@@ -1,8 +1,15 @@
 package com.example.Travel_agency.controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.Travel_agency.services.creatingAccountVerification;
 
 import entities.user;
 
+@RestController
 public class userManagmentCtr {
     private user u;
     private creatingAccountVerification createAccount;
@@ -13,13 +20,33 @@ public class userManagmentCtr {
         createAccount = new creatingAccountVerification();
     }
 
-    public void setUserDetails(String name){
-        
+    @PutMapping("/travel_agency/updateUser")
+    public void setUserDetails(
+        @RequestParam String name,
+        @RequestParam String phoneNumber,
+        @RequestParam String address,
+        @RequestParam String email,
+        @RequestParam String age) {
+        u.setName(name);
+        u.setPhoneNumber(phoneNumber);
+        u.setAddress(address);
+        u.setEmail(email);
+        u.setAge(age);
     }
     
-    public boolean createUser(){
-        return createAccount.verify(u);
+    @GetMapping("/travel_agency/createAccount")
+    public boolean createUserAccount(){
+        if(createAccount.verify(u)){
+            System.out.println("Account created");
+            return true;
+        }
+        return false;
     }
+
+    /*@GetMapping("/travel_agency/getUser")
+    public user returnUserData(){
+        return u;
+    }*/
 
 
 
--- src/main/java/com/example/Travel_agency/services/creatingAccountVerification.java
+++ src/main/java/com/example/Travel_agency/services/creatingAccountVerification.java
@@ -5,8 +5,10 @@ import entities.user;
 public class creatingAccountVerification {
     
     public boolean verify(user u){
-        
 
+        if(u.getEmail().contains("@gmail.com")){
+            return true;
+        }
         return false;
     }
 
