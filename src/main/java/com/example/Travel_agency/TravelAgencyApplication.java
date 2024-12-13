3d1f9f8f3de0a24ae886f493e28626b0bbb1b635
GEMI <ahmed.mohamedmed1111@gmail.com>
2024-12-13 23:43:56 +0200

    Intial commit

--- src/main/java/com/example/Travel_agency/TravelAgencyApplication.java
+++ src/main/java/com/example/Travel_agency/TravelAgencyApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.Travel_agency.controllers.DummyController;
-
 @SpringBootApplication
 public class TravelAgencyApplication {
 
